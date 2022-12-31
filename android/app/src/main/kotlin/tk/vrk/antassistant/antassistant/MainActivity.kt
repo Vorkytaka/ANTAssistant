c20e1f3d7fa994a2f2af8943dc8d396869a323c9
Vorkytaka <vorkytaka@gmail.com>
2022-12-31 21:30:30 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/tk/vrk/antassistant/antassistant/MainActivity.kt
@@ -0,0 +1,6 @@
+package tk.vrk.antassistant.antassistant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:antassistant/presentation/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
