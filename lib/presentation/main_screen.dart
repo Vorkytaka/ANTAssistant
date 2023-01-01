605e9b83ce720992a18d1e70e471d3925fb6ee45
Vorkytaka <vorkytaka@gmail.com>
2023-01-01 17:07:37 +0300

    Base for main screen separation

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -7,13 +7,87 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<AccountsCubit, List<String>>(
+      builder: (context, state) {
+        if (state.isEmpty) {
+          return const WelcomeScreen();
+        } else if (state.length == 1) {
+          return const AccountScreen();
+        } else {
+          return const AccountListScreen();
+        }
+      },
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      body: BlocBuilder<AccountsCubit, List<String>>(
-        builder: (context, state) => ListView.builder(
-          itemBuilder: (context, i) => Text(state[i]),
-          itemCount: state.length,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text.rich(
+                const TextSpan(
+                  children: [
+                    TextSpan(text: 'Добро пожаловать в '),
+                    TextSpan(
+                        text: 'ANTAssistant',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                style: theme.textTheme.displaySmall,
+              ),
+              const Spacer(),
+              SizedBox(
+                height: 56,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/auth');
+                  },
+                  child: const Text('Войти'),
+                ),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 56,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Звонок в службу поддержки'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class AccountListScreen extends StatelessWidget {
+  const AccountListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
