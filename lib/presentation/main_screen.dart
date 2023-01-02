3939af54998d4f9642de228c07887a00914136c5
Vorkytaka <vorkytaka@gmail.com>
2023-01-02 04:27:01 +0300

    Base for accounts list

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -295,26 +295,67 @@ class AccountListScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Аккаунты'),
+        actions: [
+          BlocSelector<AccountsCubit, AccountsState, bool>(
+            selector: (state) => state.states
+                .any((element) => element.status == AccountStatus.loading),
+            builder: (context, isLoading) {
+              return IconButton(
+                onPressed: isLoading
+                    ? null
+                    : () {
+                        context.read<AccountsCubit>().updateAll();
+                      },
+                icon: const Icon(Icons.refresh_outlined),
+              );
+            },
+          ),
+        ],
       ),
       body: BlocBuilder<AccountsCubit, AccountsState>(
         builder: (context, state) {
-          return ListView.builder(
-            physics: const ScrollPhysics(),
-            itemCount: state.states.length,
-            itemBuilder: (context, i) {
-              final current = state.states[i];
-              final data = current.data;
-              return ListTile(
-                onTap: () {},
-                title: Text(current.username),
-                subtitle: data != null
-                    ? Text('Дней осталось: ${data.daysLeft}')
-                    : const Text('Не удалось загрузить данные'),
-                trailing: data != null
-                    ? Text('${data!.balance} Р')
-                    : const SizedBox.shrink(),
-              );
-            },
+          return Column(
+            children: [
+              BlocSelector<AccountsCubit, AccountsState, bool>(
+                selector: (state) => state.states
+                    .any((element) => element.status == AccountStatus.loading),
+                builder: (context, isLoading) {
+                  if (isLoading) {
+                    return const LinearProgressIndicator(minHeight: 4);
+                  } else {
+                    return const SizedBox(height: 4);
+                  }
+                },
+              ),
+              Expanded(
+                child: ListView.builder(
+                  physics: const ScrollPhysics(),
+                  itemCount: state.states.length,
+                  itemBuilder: (context, i) {
+                    final current = state.states[i];
+                    final data = current.data;
+
+                    final String subtitle;
+                    if (data != null) {
+                      subtitle = 'Дней осталось: ${data.daysLeft}';
+                    } else if (current.status == AccountStatus.loading) {
+                      subtitle = 'Загрузка...';
+                    } else {
+                      subtitle = 'Не удалось загрузить данные';
+                    }
+
+                    return ListTile(
+                      onTap: () {},
+                      title: Text(current.username),
+                      subtitle: Text(subtitle),
+                      trailing: data != null
+                          ? Text('${data.balance} Р')
+                          : const SizedBox.shrink(),
+                    );
+                  },
+                ),
+              ),
+            ],
           );
         },
       ),
